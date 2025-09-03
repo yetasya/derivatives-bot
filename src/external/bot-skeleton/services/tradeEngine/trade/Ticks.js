e7d9611d00bd7753ba5d35666b37c5f8c11cb286
Rupato Braganza <rupato@deriv.com>
2025-09-03 15:54:54 +0800

    fix: accumulators call

--- src/external/bot-skeleton/services/tradeEngine/trade/Ticks.js
+++ src/external/bot-skeleton/services/tradeEngine/trade/Ticks.js
@@ -130,7 +130,7 @@ export default Engine =>
                 growth_rate: this?.tradeOptions?.growth_rate,
                 proposal: 1,
                 subscribe: 1,
-                symbol: this?.tradeOptions?.symbol,
+                underlying_symbol: this?.tradeOptions?.symbol,
             };
             if (!subscription_id && !is_proposal_requested) {
                 this.is_proposal_requested_for_accumulators = true;
