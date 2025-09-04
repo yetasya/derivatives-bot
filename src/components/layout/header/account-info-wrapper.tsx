d5aecf208528d6e5ed9086bb87c76e191d337cab
Prince <prince@deriv.com>
2025-09-04 11:36:46 +0400

    chore: match account switcher ui to dtrader

--- src/components/layout/header/account-info-wrapper.tsx
+++ src/components/layout/header/account-info-wrapper.tsx
@@ -1,25 +1,16 @@
 import React from 'react';
-import Popover from '@/components/shared_ui/popover';
 
 type TAccountInfoWrapper = {
     is_disabled?: boolean;
-    is_mobile?: boolean;
-    is_dtrader_v2?: boolean;
     disabled_message?: string;
+    is_mobile?: boolean;
+    children: React.ReactNode;
 };
 
-const AccountInfoWrapper = ({
-    is_disabled,
-    is_mobile,
-    disabled_message,
-    children,
-}: React.PropsWithChildren<TAccountInfoWrapper>) =>
-    is_disabled && disabled_message ? (
-        <Popover alignment={is_mobile ? 'bottom' : 'left'} message={disabled_message} zIndex='99999'>
-            {children}
-        </Popover>
-    ) : (
-        <React.Fragment>{children}</React.Fragment>
+const AccountInfoWrapper = ({ is_disabled, children }: TAccountInfoWrapper) => {
+    return (
+        <div className={`account-info-wrapper ${is_disabled ? 'account-info-wrapper--disabled' : ''}`}>{children}</div>
     );
+};
 
 export default AccountInfoWrapper;
--- src/components/layout/header/account-switcher.tsx
+++ src/components/layout/header/account-switcher.tsx
@@ -1,18 +1,72 @@
 import React from 'react';
+import classNames from 'classnames';
 import { observer } from 'mobx-react-lite';
-import { AccountSwitcher as UIAccountSwitcher } from '@deriv-com/ui';
+import { getCurrencyDisplayCode } from '@/components/shared';
+import Text from '@/components/shared_ui/text';
+import { Localize, localize } from '@deriv-com/translations';
+import { useDevice } from '@deriv-com/ui';
 import { TAccountSwitcher } from './common/types';
+import AccountInfoIcon from './account-info-icon';
+import AccountInfoWrapper from './account-info-wrapper';
 import './account-switcher.scss';
 
 const AccountSwitcher = observer(({ activeAccount }: TAccountSwitcher) => {
+    const { isDesktop } = useDevice();
+
+    if (!activeAccount) return null;
+
+    const { currency, isVirtual, balance } = activeAccount;
+    const currency_lower = currency?.toLowerCase();
+    const is_disabled = false; // Since the original had isDisabled={true}, we keep it non-interactive
+
     return (
-        activeAccount && (
-            <UIAccountSwitcher
-                buttonClassName='account-switcher-fixed'
-                activeAccount={activeAccount}
-                isDisabled={true} // Force disabled to prevent interactions
-            />
-        )
+        <div className='acc-info__wrapper'>
+            {isDesktop && <div className='acc-info__separator' />}
+            <AccountInfoWrapper is_disabled={is_disabled}>
+                <div
+                    data-testid='dt_acc_info'
+                    id='dt_core_account-info_acc-info'
+                    className={classNames('acc-info', {
+                        'acc-info--is-virtual': isVirtual,
+                        'acc-info--is-disabled': is_disabled,
+                    })}
+                >
+                    <span className='acc-info__id'>
+                        {isDesktop ? (
+                            <AccountInfoIcon is_virtual={isVirtual} currency={currency_lower} />
+                        ) : (
+                            (isVirtual || currency) && (
+                                <AccountInfoIcon is_virtual={isVirtual} currency={currency_lower} />
+                            )
+                        )}
+                    </span>
+                    <div className='acc-info__content'>
+                        <div className='acc-info__account-type-header'>
+                            <Text as='p' size='xxxs' className='acc-info__account-type'>
+                                {isVirtual ? localize('Demo') : localize('Real')}
+                            </Text>
+                        </div>
+                        {(typeof balance !== 'undefined' || !currency) && (
+                            <div className='acc-info__balance-section'>
+                                <p
+                                    data-testid='dt_balance'
+                                    className={classNames('acc-info__balance', {
+                                        'acc-info__balance--no-currency': !currency && !isVirtual,
+                                    })}
+                                >
+                                    {!currency ? (
+                                        <Localize i18n_default_text='No currency assigned' />
+                                    ) : (
+                                        `${balance} ${getCurrencyDisplayCode(currency)}`
+                                    )}
+                                </p>
+                            </div>
+                        )}
+                    </div>
+                </div>
+            </AccountInfoWrapper>
+            {isDesktop && <div className='acc-info__separator' />}
+        </div>
     );
 });
 
